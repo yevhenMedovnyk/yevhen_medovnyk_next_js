a0dd5dfc1943534411c383d36cf427f0df5fca02
Yevhen Medovnyk <yevhenmed@gmail.com>
2026-01-25 21:01:55 +0200

    fix unstable_cache for get album fn

--- src/lib/albums.ts
+++ src/lib/albums.ts
@@ -2,34 +2,35 @@ import { unstable_cache } from 'next/cache';
 import dbConnect from '@/lib/dbConnect';
 import ImageAlbum from '@/models/ImageAlbum';
 
-export const getAlbumBySlug = unstable_cache(
-	async (slug: string) => {
-		await dbConnect();
-		return ImageAlbum.findOne({ slug }).lean();
-	},
-	['album-by-slug'],
-	{
-		revalidate: 3600,
-		tags: ['Albums'],
-	}
-);
+export const getAlbumBySlug = (slug: string) =>
+	unstable_cache(
+		async () => {
+			console.log('DB HIT: album', slug);
+			await dbConnect();
+			return ImageAlbum.findOne({ slug }).lean();
+		},
+		['album-by-slug', slug],
+		{ revalidate: 3600 }
+	)();
 
-export const getAlbums = unstable_cache(
-	async (category: string) => {
-		await dbConnect();
-		const albums = await ImageAlbum.find({ category }).lean();
-		const filteredAlbums = albums.map((album) => {
-			return {
-				...album,
-				_id: album._id.toString(),
-			};
-		});
+export const getAlbums = (category: string) =>
+	unstable_cache(
+		async () => {
+			console.log('DB HIT: getAlbums', category);
+			await dbConnect();
+			const albums = await ImageAlbum.find({ category }).lean();
+			const filteredAlbums = albums.map((album) => {
+				return {
+					...album,
+					_id: album._id.toString(),
+				};
+			});
 
-		return filteredAlbums;
-	},
-	['albums'],
-	{
-		revalidate: 3600,
-		tags: ['Albums'],
-	}
-);
+			return filteredAlbums;
+		},
+		['albums', category],
+		{
+			revalidate: 3600,
+			tags: ['Albums'],
+		}
+	)();
